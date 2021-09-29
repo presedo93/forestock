993fd72c23dbcdf65a0099e4afd48bab3427b502
Rene Presedo <presedo93@gmail.com>
2021-09-29 15:59:55 +0200

    ⚡✨ Inference with ONNX working

--- infer.py
+++ infer.py
@@ -2,10 +2,11 @@ import torch
 import argparse
 import numpy as np
 import pandas as pd
+import onnxruntime as ort
 import pytorch_lightning as pl
 
-from typing import Tuple
 from tools.ta import BBANDS
+from typing import Union, Tuple
 from models.regression import LitForestockReg
 from sklearn.preprocessing import MinMaxScaler
 from tools.utils import get_checkpoint_hparams, str2bool
@@ -30,29 +31,38 @@ def get_50_last(csv_path: str) -> pd.DataFrame:
 def normalize(df: pd.DataFrame) -> Tuple[np.array, MinMaxScaler]:
     sc = MinMaxScaler()
     x = sc.fit_transform(df)
-    x = torch.tensor(x, dtype=torch.float32).T.unsqueeze(0)
 
     return x, sc
 
 
-def unnormalize(y_hat: torch.tensor, sc: MinMaxScaler) -> np.array:
-    if y_hat.device != "cpu":
-        y_hat = y_hat.cpu()
-    y_hat = y_hat.detach().numpy()
+def unnormalize(y_hat: Union[list, torch.tensor], sc: MinMaxScaler) -> np.array:
+    if type(y_hat) is not list:
+        if y_hat.device != "cpu":
+            y_hat = y_hat.cpu()
+        y_hat = y_hat.detach().numpy()
     return (y_hat - sc.min_[3]) / sc.scale_[3]
 
 
 def inference(args: argparse.Namespace) -> None:
-    check_path, hp = get_checkpoint_hparams(args.checkpoint)
-
-    forestock = LitForestockReg.load_from_checkpoint(check_path)
-    device = "cuda:0" if torch.cuda.is_available() and args.gpus else "cpu"
-    forestock.to(device)
-
-    forestock.eval()
     x, sc = normalize(get_50_last(args.data))
 
-    y_hat = unnormalize(forestock(x.to(device)), sc)
+    if args.onnx is None:
+        check_path, hp = get_checkpoint_hparams(args.checkpoint)
+
+        forestock = LitForestockReg.load_from_checkpoint(check_path)
+        device = "cuda:0" if torch.cuda.is_available() and args.gpus else "cpu"
+        forestock.to(device)
+
+        x = torch.tensor(x, dtype=torch.float32).T.unsqueeze(0)
+        forestock.eval()
+        y_hat = unnormalize(forestock(x.to(device)), sc)[0, 0]
+    else:
+        forestock = ort.InferenceSession(args.onnx, providers=["CUDAExecutionProvider"])
+        inputs = forestock.get_inputs()[0].name
+
+        x = np.expand_dims(x.T.astype(np.float32), axis=0)
+        f_inputs = {inputs: x}
+        y_hat = unnormalize(forestock.run(None, f_inputs), sc)[0, 0, 0]
 
     print(f"\033[1;32mPredicted: {y_hat}\033[0m")
 
@@ -60,6 +70,7 @@ def inference(args: argparse.Namespace) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint", type=str, help="Path to the checkpoint to load")
+    parser.add_argument("--onnx", type=str, help="ONNX model to infer with")
     parser.add_argument("--data", type=str, help="Path to the CSV data")
     parser.add_argument("--gpus", type=str2bool, help="Make use of a GPU")
 
--- onnx.py
+++ onnx.py
@@ -1,18 +1,23 @@
 import torch
 import argparse
-import pytorch_lightning as pl
 
 from models.regression import LitForestockReg
+from tools.utils import get_checkpoint_hparams
 
 
 def import_onnx(args: argparse.Namespace) -> None:
+    check_path, hp = get_checkpoint_hparams(args.checkpoint)
 
-    return
+    # Save the model
+    forestock = LitForestockReg.load_from_checkpoint(check_path)
+    sample = torch.randn((1, 8, 50))
+    forestock.to_onnx(args.onnx, sample, export_params=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint", type=str, help="Path to the checkpoint to load")
+    parser.add_argument("--onnx", type=str, help="Name of the ONNX file")
 
     args = parser.parse_args()
     import_onnx(args)
